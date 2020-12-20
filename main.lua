1ce3e5ac4e9a9d6e4346876988732dfe58b9042e
Liam <liamaiscool8@gmail.com>
2020-12-20 12:28:13 -0500

    hotfix

--- main.lua
+++ main.lua
@@ -3,12 +3,10 @@ local toolbar = plugin:CreateToolbar("Custom Script Tools")
 local directoryButton = toolbar:CreateButton("Get Directory", "Get the working directory of an instance", "rbxassetid://4458901886")
 
 local function onDirectoryButtonClick()
-for _, object in pairs (Selection:Get()) do
 
-    local directory = object:GetFullName()
-    print(directory)
+	local select = game.Selection:Get()[1]:GetFullName()
+	print(select)
+
+	end
 
 directoryButton.Click:Connect(onDirectoryButtonClick)
-
-    end
-end
