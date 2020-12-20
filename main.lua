e5da0ea3fc42079449656b8d5a85f0c0648429f9
Liam Andrzejewski <liamandrzejewski12@gmail.com>
2020-12-20 11:46:14 -0500

    init

---
+++ main.lua
@@ -0,0 +1,13 @@
+local Selection = game:GetService('Selection')
+local toolbar = plugin:CreateToolbar("Custom Script Tools")
+local directoryButton = toolbar:CreateButton("Get Directory", "Get the working directory of an instance", "rbxassetid://4458901886")
+
+local function onDirectoryButtonClick()
+for _, object in pairs (Selection:Get()) do
+    if object == Instance then
+        local directory = object:GetFullName()
+        Print(directory)
+
+    elseif print('Error, Not an instance')
+
+directoryButton.Click:Connect(onDirectoryButtonClick)
