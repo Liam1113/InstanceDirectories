e52b979afe3a0474a9f55126a8ee9b9ef603c487
Liam <liamaiscool8@gmail.com>
2020-12-28 23:25:27 -0500

    added .vscode, toml files for selene, and updated main.lua to send a message telling you your directory

--- main.lua
+++ main.lua
@@ -5,8 +5,13 @@ local directoryButton = toolbar:CreateButton("Get Directory", "Get the working d
 local function onDirectoryButtonClick()
 
 	local select = game.Selection:Get()[1]:GetFullName()
-	print('InstanceDirectories - ' + select)
-
-	end
+	local message = Instance.new('Hint')
+	message.Text='Your Directory is' .. select .. ', Check the output to copy and paste it'
+	message.Parent = workspace
+	print('InstanceDirectories - ' .. select)
+	
+	wait(6)
+	message:Destroy()
+end
 
 directoryButton.Click:Connect(onDirectoryButtonClick)
