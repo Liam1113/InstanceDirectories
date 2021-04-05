bfe909e1f37ebc09a6d0085fa4379e985cd5d341
Liam <liamandrzejewski12@gmail.com>
2021-04-05 12:14:23 -0400

    temp, rewriting commits

--- main.lua
+++ main.lua
@@ -1,17 +1,32 @@
-local Selection = game:GetService('Selection')
-local toolbar = plugin:CreateToolbar("Custom Script Tools")
-local directoryButton = toolbar:CreateButton("Get Directory", "Get the working directory of an instance", "rbxassetid://4458901886")
+	local Selection = game:GetService('Selection')
+	local toolbar = plugin:CreateToolbar("Custom Script Tools")
+	local directoryButton = toolbar:CreateButton("Get Directory", "Get the working directory of an instance", "rbxassetid://4458901886")
+	local treeButton = toolbar:CreateButton("Get Tree", "Get the tree visualizer of an instance", "rbxassetid://4458901886")
 
-local function onDirectoryButtonClick()
+	local function onDirectoryButtonClick()
 
-	local select = game.Selection:Get()[1]:GetFullName()
-	local message = Instance.new('Hint')
-	message.Text='Your Directory is' .. select .. ', Check the output to copy and paste it'
-	message.Parent = workspace
-	print('InstanceDirectories - ' .. select)
-	
-	wait(6)
-	message:Destroy()
-end
+		local select = Selection:Get()[1]:GetFullName()
+		local message = Instance.new('Hint')
+		message.Text='Your Directory is' .. select .. ', Check the output to copy and paste it'
+		message.Parent = workspace
+		print('InstanceDirectories - ' .. select)
+		
+		wait(6)
+		message:Destroy()
+	end
 
-directoryButton.Click:Connect(onDirectoryButtonClick)
+	local function createSortedHierarchy(ancestor)
+		local function convertChildrenToTable(parent)
+			local children = {}
+			for _, child in pairs(parent:GetChildren()) do
+				children[child.Name] = convertChildrenToTable(child)
+			end
+			return children
+		end
+		return {[ancestor.Name] = convertChildrenToTable(ancestor)}
+	end
+
+	print(createSortedHierarchy(Selection:Get()))
+
+	directoryButton.Click:Connect(onDirectoryButtonClick)
+	treeButton.Click:Connect(createSortedHierarchy)
